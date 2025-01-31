ba8e3652143343b70d2bb4072e0d4195f973905e
jhyotsna <jhyotsnasrinivas@gmail.com>
2025-01-31 21:33:42 +0530

    first commit

---
+++ src/Components/HomeFooter.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const HomeFooter = () => {
+  return (
+    <div className="home-footer">
+      <div className="home-footer-content">
+        <p>&copy; 2025 RGUKT Srikakulam Campus. All rights reserved.</p>
+        <div className="home-footer-links">
+          <a href="https://www.rgukt.ac.in" target="_blank" rel="noopener noreferrer">
+            Official Website
+          </a>
+          <a href="https://www.facebook.com/RGUKT" target="_blank" rel="noopener noreferrer">
+            Facebook
+          </a>
+          <a href="https://twitter.com/RGUKT" target="_blank" rel="noopener noreferrer">
+            Twitter
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeFooter;
+
