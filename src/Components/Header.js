c8d7a3046079f7a294d40a4c3ad0cc4213b63539
jhyotsna <jhyotsnasrinivas@gmail.com>
2025-01-31 21:33:14 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header=(props)=>{
+    return(
+        <div class="Header-class">
+                <h2>{props.name}</h2>
+        </div>
+    );
+}
+export default Header;
